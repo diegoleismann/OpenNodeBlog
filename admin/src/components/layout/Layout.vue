c261a37e7f643563c45fc2c2f6f4fb7fbfc6855f
Diego L <diegoleismann@gmail.com>
2025-08-28 14:21:47 -0300

    Formatado arquivos

--- admin/src/components/layout/Layout.vue
+++ admin/src/components/layout/Layout.vue
@@ -1,20 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
 <template>
-  <v-layout>
+  <v-layout class="h-100">
     <v-navigation-drawer permanent>
       <v-list nav>
-        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/61.jpg" subtitle="Admin" title="Fulano"
-          append-icon="mdi-chevron-down" value="account"></v-list-item>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/61.jpg"
+          subtitle="Admin"
+          title="Fulano"
+          append-icon="mdi-chevron-down"
+          value="account"
+        ></v-list-item>
       </v-list>
 
-      <v-divider></v-divider>
-
-      <v-list density="compact" nav>
-        <v-list-item active-class="bg-blue text-white" prepend-icon="mdi-view-grid" title="Dashboard"
-          value="dashboard"></v-list-item>
-        <v-list-item active-class="bg-blue text-white" prepend-icon="mdi-list-box" title="Posts"
-          value="shared"></v-list-item>
-        <v-list-item active-class="bg-blue text-white" prepend-icon="mdi-account" title="Users"
-          value="users"></v-list-item>
+      <v-list
+        density="compact"
+        nav
+      >
+        <v-list-item
+          active-class="bg-blue text-white"
+          prepend-icon="mdi-view-grid"
+          title="Dashboard"
+          value="dashboard"
+        ></v-list-item>
+        <v-list-item
+          active-class="bg-blue text-white"
+          prepend-icon="mdi-list-box"
+          title="Posts"
+          value="shared"
+        ></v-list-item>
+        <v-list-item
+          active-class="bg-blue text-white"
+          prepend-icon="mdi-account"
+          title="Users"
+          value="users"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
--- admin/src/views/dashboard/dashboardPage.vue
+++ admin/src/views/dashboard/dashboardPage.vue
@@ -6,42 +6,43 @@
           <v-card-title>
             <v-toolbar class="bg-white">
               <v-title>Posts</v-title>
-              <v-divider class="mx-2" vertical opacity="0"></v-divider>
+              <v-divider
+                class="mx-2"
+                vertical
+                opacity="0"
+              ></v-divider>
               <v-btn class="bg-blue text-white">Novo</v-btn>
             </v-toolbar>
           </v-card-title>
-          <v-data-table :items="listPosts">
-
-          </v-data-table>
+          <v-data-table :items="listPosts"> </v-data-table>
         </v-card>
       </v-col>
     </v-row>
   </Layout>
 </template>
 <script setup>
-import { ref } from 'vue';
-import Layout from '../../components/layout/Layout.vue';
+import { ref } from 'vue'
+import Layout from '../../components/layout/Layout.vue'
 const listPosts = ref([
   {
-    'created_at': '10/12/2030 10:15',
-    'title': 'title',
-    'url': '/post/title'
+    created_at: '10/12/2030 10:15',
+    title: 'title',
+    url: '/post/title',
   },
   {
-    'created_at': '10/12/2030 10:15',
-    'title': 'title',
-    'url': '/post/title'
+    created_at: '10/12/2030 10:15',
+    title: 'title',
+    url: '/post/title',
   },
   {
-    'created_at': '10/12/2030 10:15',
-    'title': 'title',
-    'url': '/post/title'
+    created_at: '10/12/2030 10:15',
+    title: 'title',
+    url: '/post/title',
   },
   {
-    'created_at': '10/12/2030 10:15',
-    'title': 'title',
-    'url': '/post/title'
+    created_at: '10/12/2030 10:15',
+    title: 'title',
+    url: '/post/title',
   },
-
 ])
 </script>
